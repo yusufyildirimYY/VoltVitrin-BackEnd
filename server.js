ca88b09c75bff8fb086d20167935364cd3890278
DouglassY <teryakiboyzv2@gmail.com>
2024-06-07 02:04:28 +0300

    trying to fix heroku error-3

--- server.js
+++ server.js
@@ -1,38 +1,36 @@
 const express = require("express");
 const cors = require("cors");
-// const { readItems, readBrands } = require("./Database/database");
+var timeout = require("connect-timeout");
+
+const { readItems, readBrands } = require("./Database/database");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
+app.use(timeout("60s"));
 app.get("/", (req, res) => {
-  const text = "test";
-  res.send(test);
+  const type = req.query.type;
+  switch (type) {
+    case "all":
+      readItems((err, rows) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.send(rows);
+        }
+      });
+      break;
+    case "brands":
+      readBrands((err, rows) => {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.send(rows);
+        }
+      });
+      break;
+  }
 });
-// app.get("/", (req, res) => {
-//   const type = req.query.type;
-//   switch (type) {
-//     case "all":
-//       readItems((err, rows) => {
-//         if (err) {
-//           res.status(500).send(err);
-//         } else {
-//           res.send(rows);
-//         }
-//       });
-//       break;
-//     case "brands":
-//       readBrands((err, rows) => {
-//         if (err) {
-//           res.status(500).send(err);
-//         } else {
-//           res.send(rows);
-//         }
-//       });
-//       break;
-//   }
-// });
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("server is running");
