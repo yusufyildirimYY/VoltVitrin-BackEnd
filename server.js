c1b7eebfdb66fe69ea8b7149dd1bbc58e4d0fbe4
DouglassY <teryakiboyzv2@gmail.com>
2024-06-07 12:53:38 +0300

    fix

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.get("/api", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("server is running");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
